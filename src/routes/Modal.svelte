b67fb2b6a59424012db484aa5c7140c58c194235
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-12-11 16:55:52 +0000

    Added Modal.svelte

---
+++ src/routes/Modal.svelte
@@ -0,0 +1,19 @@
+<div class="modal-background">
+  <slot />
+</div>
+
+<style>
+  .modal-background {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    display: grid;
+    place-items: center;
+    backdrop-filter: blur(10px);
+    z-index: 999;
+    transition: all .2s;
+    text-align:center
+  }
+</style>
